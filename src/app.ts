8cda6af028349b4c6ac7d3eb0dcdc09a6d64ba47
Abdulrahman-M-Darwish <winding192837456456@gmail.com>
2022-10-23 00:15:22 +0200

    resbumition

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,42 @@
 import express from "express";
-import path from "path";
-import sharp from "sharp";
+import resize from "./utils/resize";
 
 const app = express();
 
-app.get("/", (req, res) =>
-  res.send("<h1>welcome to my image resizer app</h1>")
+app.get(
+  "/",
+  (req: express.Request, res: express.Response): express.Response =>
+    res.send("<h1>welcome to my image resizer app</h1>")
 );
 
-app.get("/resize", async (req, res) => {
-  const { filename, width, height } = req.query;
-  if (!filename) {
-    return res.status(404).send("please fill the filename");
+app.get(
+  "/resize",
+  async (
+    req: express.Request,
+    res: express.Response
+  ): Promise<express.Response | undefined> => {
+    const { filename, width, height } = req.query;
+    try {
+      if (!width || !height) {
+        throw new Error("please fill the dimensions");
+      }
+      const imagePath = await resize(filename as string, +width, +height);
+      res.sendFile(imagePath);
+    } catch (err) {
+      if ((err as Error).message.includes("Input file is missing")) {
+        return res
+          .status(404)
+          .send(
+            `there is no image with name: ${filename}.jpg, please make sure you wrote the right name`
+          );
+      }
+      res.status(404).send((err as Error).message);
+    }
   }
-  if (!width || !height) {
-    return res.status(404).send("please fill the dimensions");
-  }
-  await sharp(path.resolve("./") + "/images/" + filename + ".jpg")
-    .resize({
-      width: +width,
-      height: +height,
-    })
-    .toFile(path.resolve("./") + "/output/" + "new-" + filename + ".jpg");
-  res.sendFile(path.resolve("./") + "/output/" + "new-" + filename + ".jpg");
+);
+
+app.use((req: express.Request, res: express.Response): void => {
+  res.status(404).send("<h1>Oops >_<, 404 page not found</h1>");
 });
 
 app.listen(3000);
